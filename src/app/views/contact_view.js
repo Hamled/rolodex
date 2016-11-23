d5588e55f5cfe73d3a38973e562f8347e6e61790
Charles Ellis <hamled@hamled.name>
2016-11-23 15:24:25 -0800

    Wave 3: Details modal goes away

--- src/app/views/contact_view.js
+++ src/app/views/contact_view.js
@@ -12,6 +12,11 @@ const ContactView = Backbone.View.extend({
 
   onClick: function(e) {
     this.trigger('select', this);
+
+    // We return false to tell jQuery not to run any more event handlers.
+    // Otherwise, it would run the 'click' event handler on RolodexView
+    // as well.
+    return false;
   },
 
   render: function() {
--- src/app/views/rolodex_view.js
+++ src/app/views/rolodex_view.js
@@ -11,6 +11,31 @@ const RolodexView = Backbone.View.extend({
     this.listenTo(this.model, 'update', this.render);
   },
 
+  events: {
+    'click': 'onClick'
+  },
+
+  onClick: function(e) {
+    // There are many ways to implement the logic for hiding a modal.
+    // In this case I've handled the possibility of clicking anywhere on
+    // the RolodexView's controlled area (the whole right side of the page)
+    // and if we didn't click specifically on the modal, then we consider
+    // that as choosing to close it.
+    // Another popular way to implement this is to overlay a shaded,
+    // transparent div on top of the entire page, and then display the modal
+    // on top of it. When the overlay div is clicked upon, then the modal
+    // goes away.
+
+    // The actual DOM element for the modal
+    const modalElement = this.detailsModal[0];
+    const clickedOnModal = (modalElement == e.target ||
+                            Backbone.$.contains(modalElement, e.target));
+
+    if(this.detailsModal.is(':visible') && !clickedOnModal) {
+      this.detailsModal.hide();
+    }
+  },
+
   showCard: function(card) {
     const cardDetails = this.detailsTemplate(card.model.attributes);
     this.detailsModal.html(cardDetails);
