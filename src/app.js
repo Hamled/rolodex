2d6efdb1a1d46fb0f4067dcf88b1f5b89497739e
Charles Ellis <hamled@hamled.name>
2016-11-23 13:40:49 -0800

    Wave 2: Use Rolodex collection

--- src/app.js
+++ src/app.js
@@ -1,7 +1,35 @@
 import Application from 'app/models/application';
 import ApplicationView from 'app/views/application_view';
 
-var application = new Application();
+var application = new Application({
+  contacts: [
+    {
+      name: "Charles",
+      email: "charles@example.com",
+      phone: "206-555-1234"
+    },
+    {
+      name: "Kari",
+      email: "kari@example.com",
+      phone: "206-555-1236"
+    },
+    {
+      name: "Dan",
+      email: "dan@example.com",
+      phone: "206-555-1235"
+    },
+    {
+      name: "Jamie",
+      email: "jamie@example.com",
+      phone: "206-555-1238"
+    },
+    {
+      name: "Chris",
+      email: "chris@example.com",
+      phone: "206-555-1237"
+    }
+  ]
+});
 
 var appView = new ApplicationView({
   el: '#application',
--- src/app/models/application.js
+++ src/app/models/application.js
@@ -1,10 +1,15 @@
 import Backbone from 'backbone';
 import Contact from 'app/models/contact';
+import Rolodex from 'app/collections/rolodex';
 
 const Application = Backbone.Model.extend({
   // This model represents the overall application.
-  initialize: function() {
+  initialize: function(options) {
     this.contact = new Contact();
+    var contacts = options.contacts.map(function(attrs) {
+      return new Contact(attrs);
+    });
+    this.rolodex = new Rolodex(contacts);
   }
 });
 
